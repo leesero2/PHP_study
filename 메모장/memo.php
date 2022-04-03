b6e76de3db339d14bf980502c2b537b2d809093a
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-04-03 23:10:35 +0900

    php를 이용한 메모장 만들기
    php를 이용한 첫번째 프로젝트

---
+++ 메모장/memo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>메모장</title>
+</head>
+<body>
+    <div>
+        <table border = "1">
+            <tr>
+                <td>
+</table>
+</div>    
+
+
+
+</body>
+</html>
