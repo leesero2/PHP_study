3c5857e66e0048500e23869ca32377b27ba6e81e
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-04-05 17:54:15 +0900

    php문법 include문

---
+++ include문/9.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        윤딴딴 - 휴가철 도로위 <br>
+        막히는 자동차 안에서 우린 아무말이 없었지 ~
+</div>
+
+<?php
+    include "9_1.php";
+    ?>
+</body>
+</html>
