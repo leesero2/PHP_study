b6c1bda39be14a48dbaa5074ed69c975a838d4e3
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-04-05 17:27:23 +0900

    Update memoSave.php

--- 메모장/memoSave.php
+++ 메모장/memoSave.php
@@ -3,6 +3,7 @@
 
     $content = $_REQUEST['name']."///".$_REQUEST['email']."///".$_REQUEST['memo']."\r\n";
     // "\r\n" - 줄바꾸기 기호
+    //memo.txt에 저장되는 내용이 줄바꿈이되서 저장이 됨
 
     echo $content; //'이지훈///huny10000@naver.com///안녕하세요 저는 이지훈이라고 합니다' 출력이됨
 
@@ -11,3 +12,8 @@
     //print_r($_REQUEST); 어떤 값을 받는지 확인하기 위해 잠시 적음
 
     fclose($fp);
+?>
+
+<script>
+    location.href='memo.php'; //작성후 memo.php로 다시 이동하라는 소스
+</script>
