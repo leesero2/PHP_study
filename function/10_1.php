81c3df2065c78d1cbf42e4c3ee8f6930668dd796
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-04-06 16:51:25 +0900

    함수 반환(return)

---
+++ function/10_1.php
@@ -0,0 +1,11 @@
+<?php
+    echo "막히는 자동차 안에서 우린 아무말이 없었지";
+    
+    $a = loopPart();
+    echo $a;
+
+    function loopPart(){
+        return "<div>그냥 마주보고 웃었지 도로는 뚫릴 생각이 없지</div>";
+    }
+?>
+
