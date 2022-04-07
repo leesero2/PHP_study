3e36b53fe38094d64ffa19e90479775a2acee2b8
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-04-07 18:21:21 +0900

    Update 20.php

--- 사용자 및 db, 테이블 만들기/20.php
+++ 사용자 및 db, 테이블 만들기/20.php
@@ -24,20 +24,21 @@
     //print_r($result); // 결과 - mysqli_result Object ( [current_field] => 0 [field_count] => 4 [lengths] => [num_rows] => 1 [type] => 0 )
     
     $a = mysqli_fetch_assoc($result); //데이터베이스 테이블안의 값을 배열로 전환해서 출력시켜주는 함수
-    
-    print_r($a); //Array ( [No] => 1 [001] => 2 [002] => 3 [003] => 4 ) 1 첫번째 줄 데이터베이스 값이 출력됨
+    //print_r($a); //Array ( [No] => 1 [001] => 2 [002] => 3 [003] => 4 ) 1 첫번째 줄 데이터베이스 값이 출력됨
 
     echo $a[No]; //배열안의 주소값은 테이블 이름을 넣어야함
-    echo $a[001];
-    echo $a[002];
-    echo $a[003];
+    echo $a[a];
+    echo $a[b];
+    echo $a[c];
+    //테이블 이름을 숫자로 했더니 출력이 안됨. 연구 필요
 
     echo "<hr>";
-
     //데이터 값이 2줄이면 한번더 출력하면 됨
-    // echo $a[No];
-    // echo $a[001];
-    // echo $a[002];
-    // echo $a[003];
+
+    $a = mysqli_fetch_assoc($result); //이 함수도 한번 더 넣어야함
+    echo $a[No];
+    echo $a[a];
+    echo $a[b];
+    echo $a[c];
 
     // print_r($connect); 잘 되었는지 확인
