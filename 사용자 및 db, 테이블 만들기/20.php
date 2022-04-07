3b5bf5ea269d4896e675903b02abfe28e5da147d
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-04-07 18:13:01 +0900

    php 데이터베이스 연결

---
+++ 사용자 및 db, 테이블 만들기/20.php
@@ -0,0 +1,31 @@
+<?php
+    //warning에러는 작동되는데 문제가 되진않음 다만, 계속 화면에 출력됨.
+    //에러가 났다고 표시만 하고싶은데 warning이랑 같이 표기된다면 아래 소스를 작성하면됨.
+
+    error_reporting(1);
+    ini_set("display_errors",1);
+    //이렇게 작성하면 warning에러가 발생하지 않음
+
+    $connect = mysqli_connect("localhost","korea","korea123","korea");
+    //순서대로 데이터베이스 호스트, 아이디, 비밀번호, 데이터베이스명 순으로 작성해야함 
+
+    //만약 아이디 및 패스워드가 옳지 않아서 에러가 발생했을때
+    if(mysqli_connect_error()){
+        echo "mysql 접속중 오류가 발생하였습니다.";
+        echo mysqli_connect_error(); //어떤 에러가 발생했는지 출력해줌
+    }
+
+    $query = "select * from memo"; //메모 테이블을 선택 후 $query 변수에 저장
+    
+    $result = mysqli_query($connect,$query);
+
+    //print_r($result); // 결과 - mysqli_result Object ( [current_field] => 0 [field_count] => 4 [lengths] => [num_rows] => 1 [type] => 0 )
+    
+    $a = mysqli_fetch_assoc($result);
+    
+    echo $a[no];
+    echo $a[1];
+    echo $a[2];
+    echo $a[3];
+
+    // print_r($connect); 잘 되었는지 확인
