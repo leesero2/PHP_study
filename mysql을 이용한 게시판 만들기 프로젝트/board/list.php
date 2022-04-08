d28f7c25c3f20b7907cd354e0195e3b26f42e000
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-04-08 20:56:58 +0900

    게시글 list화면 구현

--- mysql을 이용한 게시판 만들기 프로젝트/board/list.php
+++ mysql을 이용한 게시판 만들기 프로젝트/board/list.php
@@ -2,14 +2,40 @@
     include "lib.php";
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <table width = 800 border = "1">
     <tr>
-        <td>No</td>
-        <td>제목</td>
-        <td>작성자</td>
-        <td>작성 시간</td>
-        <td>ip</td>
+        <th width=50> No </th>
+        <th > 제목 </th>
+        <th width=100> 작성자 </th>
+        <th width=100> 작성시간 </th>
+        <!-- <td>ip</td> -->
     </tr>
+
+    <?php
+        $query = "select * from sing_board order by idx desc";
+        $result = mysqli_query($connect, $query);
+
+        while($data = mysqli_fetch_array($result)){
+    ?>
+            <tr>
+                <td> <?=$data['idx']?> </td>
+                <td> <?=$data['subject']?> </td>
+                <td> <?=$data['name']?> </td>
+                <td> <?=$data['regdate']?> </td>
+        </tr>
+        <?php } ?>
 </table>
 
 <a href = "write.php">글쓰기</a>
+</body>
+</html>
--- mysql을 이용한 게시판 만들기 프로젝트/board/write.php
+++ mysql을 이용한 게시판 만들기 프로젝트/board/write.php
@@ -6,15 +6,15 @@
     <!-- 이번엔 post방식으로 데이터를 넒겨봄. get방식은 주소창에 데이터값이 다 보여서 해킹 위험이 있음 -->
 <table width = 800 border = "1" cellpadding=5>
     <tr>
-        <td>이름</td>
+        <th>이름</th>
         <td> <input type="text" name="name" ></td>
     </tr>
     <tr>
-        <td>제목</td>
+        <th>제목</th>
         <td> <input type="text" name="subject" style="width:100%;" ></td>
     </tr>
     <tr>
-        <td>내용</td>
+        <th>내용</th>
         <td> <textarea name="memo" style="width:100%; height:300px;"></textarea></td>
     </tr>
        
