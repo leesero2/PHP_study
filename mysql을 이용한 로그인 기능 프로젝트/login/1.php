413138c22599200dc6f943d30f0d79ddc8af13c8
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-04-12 23:04:50 +0900

    mysql을 이용한 로그인 기능

---
+++ mysql을 이용한 로그인 기능 프로젝트/login/1.php
@@ -0,0 +1,5 @@
+<?php
+    setcookie("assa","안녕하세요. 123123123123".time() );
+
+    echo $_COOKIE['assa'];
+?>
---
+++ mysql을 이용한 로그인 기능 프로젝트/login/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    로그인 테스트
+    <?php
+        $a = 100;
+    ?>
+
+    <a href="text.php?a=200">페이지 이동</a> <?=$a?> <br>
+    <a href="1.php">쿠키생성</a> <br>
+    <a href="2.php">쿠키출력</a> <br>
+    <a href="3.php">쿠키출력</a> <br>
+</body>
+</html>
