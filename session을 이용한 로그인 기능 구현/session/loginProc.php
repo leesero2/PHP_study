d80a3326735bf7468c4552af69631db501f3e00c
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-04-13 20:26:58 +0900

    Update loginProc.php

--- session을 이용한 로그인 기능 구현/session/loginProc.php
+++ session을 이용한 로그인 기능 구현/session/loginProc.php
@@ -10,24 +10,24 @@
     $uid = mysqli_real_escape_string($connect,$uid);
     $pwd = mysqli_real_escape_string($connect,$pwd);
 
-    $query = "select * from members where uid = '$uid' and pwd = password('$pwd') ";
+    $query = "select * from members where uid = '$uid' and pwd = '$pwd' ";
     $result = mysqli_query($connect,$query);
     $data = mysqli_fetch_array($result);
 
 
     print_r($data);
 
-    // if($data){
-    //     $_SESSION['isLogin'] = time();
-    //     ?>
-    //     <script>
-    //         location.href='list.php';
-    //     </script>
-    //     <?php
+    if($data){
+        $_SESSION['isLogin'] = time();
+        ?>
+        <script>
+            location.href='list.php';
+        </script>
+        <?php
 
-    // }else{
+    }else{
 
-    //     echo "로그인 정보가 올바르지 않습니다.";
-    // }
+        echo "로그인 정보가 올바르지 않습니다.";
+    }
 ?>
 
