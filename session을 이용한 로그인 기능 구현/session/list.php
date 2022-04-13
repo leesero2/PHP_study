cce7394770b9a26b2b4eed5c5e5672bb21f8c1c2
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-04-13 20:31:46 +0900

    로그아웃 기능 구현

--- session을 이용한 로그인 기능 구현/session/list.php
+++ session을 이용한 로그인 기능 구현/session/list.php
@@ -1,3 +1,15 @@
+<?php
+    include "lib.php";
+
+    $isLogin = $_SESSION['isLogin'];
+
+    if(!$isLogin){
+        echo "회원만 접근 가능합니다.";
+        exit;
+    }
+
+    ?>
+        <a href="logOut.php">로그아웃</a>
 <h1>회원리스트</h1>
 <ul>
     <? for($i = 0; $i<=100; $i++){?>
---
+++ session을 이용한 로그인 기능 구현/session/logOut.php
@@ -0,0 +1,5 @@
+<?php
+include "lib.php";
+
+$_SESSION['isLogin']="";
+echo "로그아웃 되었습니다.";
