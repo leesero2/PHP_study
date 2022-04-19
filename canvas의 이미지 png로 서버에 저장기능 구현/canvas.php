3ea27dbbe96848acee4de0987b688db23ea8905e
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-04-19 20:20:32 +0900

    캔버스 이미지를 서버에 저장하는 기능 구현

--- canvas의 이미지 png로 서버에 저장기능 구현/canvas.php
+++ canvas의 이미지 png로 서버에 저장기능 구현/canvas.php
@@ -1,23 +1,49 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ko">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
         .canvas{border:1px solid #aaa; }
     </style>
     <script>
+
+    var canvas; 
+    var ctx; 
+
         function draw(){
-        var canvas = document.getElementById("a");
-        if(canvas.getContext){
-            alert('그림 그리기 가능')
+              canvas = document.getElementById("a"); 
+
+            if(canvas.getContext){
+               
+
+                ctx = canvas.getContext("2d"); 
+                
+                ctx.fillStyle = "rgb(255,0,0)"; 
+                ctx.fillRect(5,5,100,100); 
+
+                ctx.fillStyle = "rgb(0,255,0)"; 
+                ctx.fillRect(300,5,50,50); 
+
+            }
         }
-    }
-        </script>
+
+        function saveImage(){
+            var a = canvas.toDataURL("image/png"); 
+            document.getElementById("image").value = a; 
+            document.getElementById("frm").submit(); 
+        }
+
+    </script>
 </head>
 <body onload="draw();">
-    <canvas id="a" width="500" height="500" class="canvas"></canvas>
+    
+    <canvas id="a" width="500" height="500" class="canvas"></canvas> 
+    <form action="canvasSave.php" method="post" id="frm">
+        <input type="hidden" name="image" id="image" value=""> 
+        <button type="button" onclick="saveImage();">이미지 서버에 업로드 </button> 
+    </form>
+
 </body>
 </html>
