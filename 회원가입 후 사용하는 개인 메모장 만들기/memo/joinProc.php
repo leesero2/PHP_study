7b088914c53f6e5f8a0df2ee0e43defca1e9bd7f
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-04-20 19:24:44 +0900

    로그인 기능 구현

--- 회원가입 후 사용하는 개인 메모장 만들기/memo/joinProc.php
+++ 회원가입 후 사용하는 개인 메모장 만들기/memo/joinProc.php
@@ -1,13 +1,26 @@
-<?php
-    include "dbClass.php";
+<?
+
+    include "dbClass.php"; 
+
     
-    $data = array($_POST['user_id'],$_POST['pwd'],$_POST['name'],$_POST['eamil']);
-    //print_r($data);
-    // $data[] = date("Y-m-d H:i:s");
-    // $data[] = $_SERVER('REMOTE_ADDR');
+    $data = array($_POST['user_id'],$_POST['pwd'],$_POST['name'],$_POST['email']); 
+
+    $data[] = date("Y-m-d H:i:s");
+    $data[] = $_SERVER['REMOTE_ADDR']; 
 
 
-    $query = "insert into uni_member(user_id, pwd, name, email, regdate, ip) values(?,?,?,?,?,?)";
-    $db->query($query, $data);
-    
+
+    $query = "insert into uni_member(user_id, pwd, name, email, regdate, ip) values(?,?,?,?,?,?) ";
+    $insert  = $db->query($query,$data); 
+    $cnt = $insert->affectedRows();
+
+    if($cnt!=1){
+        echo $cnt;
+        exit; 
+    }
+
+    Header("Location: index.php"); 
+
+
+
 ?>
